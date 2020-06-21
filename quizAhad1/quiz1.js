84664774ef15538857e13744d23897fcb3aeb302
Fajrizky <muhammadfajrizky@gmail.com>
2020-06-21 13:26:45 +0700

    quiz

---
+++ quizAhad1/quiz1.js
@@ -0,0 +1,24 @@
+str = "Rever_1_Test_2";
+str.replace("e", "");
+
+console.log(str);
+
+// //no 5
+// function bubbleSort(array) {
+//   var done = false;
+//   while (!done) {
+//     done = true;
+//     for (var i = 1; i < array.length; i += 1) {
+//       if (array[i - 1] > array[i]) {
+//         done = false;
+//         var tmp = array[i - 1];
+//         array[i - 1] = array[i];
+//         array[i] = tmp;
+//       }
+//     }
+//   }
+//   return array;
+// }
+// const numbers = [1, 4, 5, 1, 2, 10, 12, 15, 11, 13, 11, 5];
+// bubbleSort(numbers);
+// console.log(numbers);
